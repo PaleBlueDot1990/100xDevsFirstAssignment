ef7c4d404679d6c11484f9bc6bc66b99f801130f
Bhuvnesh <bhuvnesh.bhuwan.golden@gmail.com>
2024-10-20 16:18:27 +0530

    Solved part of async-js questions

--- 02-async-js/easy/read-from-file.js
+++ 02-async-js/easy/read-from-file.js
@@ -10,18 +10,36 @@ function printFile(err, data) {
 
 fs.readFile('a.txt', 'utf8', printFile);
 
+var counter=0;
+for (var i=0; i<10000000000; i++) {
+    counter=counter+1;
+}
+
 /*
-The first argument of fs.readFile has to be the
+1.The first argument of fs.readFile has to be the
 path name of the file to be read. The second
 argument has to be the encoding format of the
 file. And the third argument needs to be the
 callback function that is called once the file
 is read using the fs.readFile() function.
 
-By definition, the callback function (which in
+2.By definition, the callback function (which in
 this case is printFile) has to take two parameters-
 err and data. err contains details of any error
 encountered while trying to read the data. And
 data contains the content of the file (if the
 file is successfully read).
+
+3.As you can see, a very expensive time consuming
+synchronous process has also been added in this
+file- the for loop which increments the counter.
+When you run this file, you will see a significant
+delay in the printing of file contents. This is
+because the after the fs.readFile API has run
+and the printFile function has been put in the
+callback queue, the event loop is waiting for this
+expensive for loop to finish so that printFile
+function can be pushed to the main stack. And
+this wait time is significant because the for loop
+take a significant time to run. 
 */
